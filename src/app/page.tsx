ed2ccc7e83983c038257cfba54fbbdd7e0a54ba3
yoricktf <yorick.tenfeld@gmail.com>
2025-01-30 12:57:54 +0100

    functional app including showing loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 'use client';
-
+import Image from 'next/image';
+import SkeletonLoader from './components/SkeletonLoader';
 import { connectOpenAi } from './actions';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 
 export default function HomePage() {
-  // const [text, setText] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [isPending, startTransition] = useTransition();
   const [audioUrl, setAudioUrl] = useState(null);
   const [podcast, setPodcast] = useState();
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (formData) => {
-    try {
-      const resultAi = await connectOpenAi(formData);
-      const podcastObject = await JSON.parse(
-        resultAi.message.choices[0].message.content
-      );
-      await handleGenerateVoice(podcastObject.content);
-      setPodcast(podcastObject);
-      setErrorMessage('');
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
+    startTransition(async () => {
+      try {
+        const resultAi = await connectOpenAi(formData);
+        // console.log(resultAi.message.choices[0].message.content);
+        const podcastObject = await JSON.parse(
+          resultAi.message.choices[0].message.content
+        );
+        setPodcast(podcastObject);
+
+        await handleGenerateVoice(podcastObject.content);
+        setErrorMessage('');
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    });
   };
 
   const handleGenerateVoice = async (podcastArray) => {
@@ -29,8 +33,6 @@ export default function HomePage() {
       alert('Please enter some podcastArray.');
       return;
     }
-    console.log('generate speech:::::::::::::::::', podcastArray);
-    setLoading(true);
     setAudioUrl(null);
     try {
       const response = await fetch('/api/elevenlabs', {
@@ -44,15 +46,12 @@ export default function HomePage() {
       if (!response.ok) {
         throw new Error('Failed to generate audio');
       }
-
       const blob = await response.blob();
       const url = URL.createObjectURL(blob);
       setAudioUrl(url);
     } catch (error) {
       console.error(error);
       alert('Error generating audio. Please try again.');
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -60,23 +59,34 @@ export default function HomePage() {
     <>
       <form action={handleSubmit}>
         <input name='prompt' placeholder='Enter your prompt' />
-        <button type='submit'>Submit</button>
+        <button type='submit' disabled={isPending}>
+          Submit
+        </button>
       </form>
+      {isPending && <SkeletonLoader />}
       {podcast && (
         <div>
           <h1>{podcast?.title}</h1>
-          <h3>Response:</h3>
           {podcast?.content?.map((message, index) => (
-            <p key={index}>
-              {message.host}: {message.message}
-            </p>
+            <div className={`textbox ${message.host}`} key={index}>
+              <Image
+                src={`/${message.host}.jpg`}
+                alt={`${message.host}'s image`}
+                width={50}
+                height={50}
+                className='avatar'
+              />
+              <p>
+                {message.host}: {message.message}
+              </p>
+            </div>
           ))}
           {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         </div>
       )}
       {audioUrl && (
         <div style={{ marginTop: '20px' }}>
-          <h2>Generated Audio</h2>
+          <h2>Your Personal Podcast</h2>
           <audio controls src={audioUrl}></audio>
         </div>
       )}
